2db043f90ce1333e0ca2c7754a776ea42dec69b9
PMMAraujo <pedro10araujo@gmail.com>
2020-07-09 08:32:15 +0100

    readme and spelling

--- recombination_classifier/streamlit.py
+++ recombination_classifier/streamlit.py
@@ -9,9 +9,9 @@ from recombination_classifier import predict
 # Title
 st.title("HIV-1 recombination classifier")
 st.write("This is a small example of a HIV-1 recombination classifier using a\
-    small neural net to train codons (triplets) based embeddings. This should\
-    not be used for other that demonstrating porpuses. Please notice that the\
-        training dataset for this classifier doesn't include subtype B data.")
+small neural net to train codon (triplets/trigrams) based embeddings. This\
+should not be used for other than demonstrating purposes. Please notice that\
+the training dataset for this classifier doesn't include subtype B data.")
 
 
 # Get run components for prediction
@@ -45,27 +45,25 @@ if page == 'Classifier':
 elif page == 'Documentation':
 
     st.header("Preprocessing")
-    st.write("""From the inputed genome the set of possible trigrams (ngrams of\
-        length 3) are created. Covering the 3 possible open reading frames.Each\
-        trigrams were then tokenized using keras. Since genome may have diferent\
-        lengths the tokenized inputs are then padded to length 3000. This data\
-        in then passed on the neural net showed in the next step. This net,\
-        containing an embedding layer, was trained on 103476 genomes (none B\
-        subtype) to classify HIV-1 genomes as recombinant or not.""")
+    st.write("""From the imputed genome the set of possible trigrams (ngrams of\
+length 3) are created. Covering the 3 possible open reading frames. Each\
+trigrams were then tokenized using keras. Since the sequenced region of the\
+genome may have different lengths the tokenized inputs are then padded to\
+length 3000. This data in then passed on the neural net showed in the next\
+step. This net, containing an embedding layer, was trained on 103476 genomes\
+(none B subtype) to classify HIV-1 genomes as recombinant or not.""")
 
     st.header("Classifier Neural Net Structure")
     st.markdown("![Alt Text](https://raw.githubusercontent.com/PMMAraujo/HIV-1_recombination_classifier/master/recombination_classifier/models_files/recomb_classifier_net.png)")
-#    st.write("""[<img src="https://github.com/madewithml/images/blob/master/images/yt.png?raw=true" style="width:1.2rem;"> Watch Lesson](https://www.youtube.com/madewithml?sub_confirmation=1)]""")
-#    st.write('[<img src="http://www.google.com.au/images/nav_logo7.png">](http://google.com.au/)]')
 
 
     st.header("Notes")
     st.write("The data set used can be found [here.](https://drive.google.com/file/d/1-Tim99TrSR8pGzLse3yN9sBBs08bFIlJ/view?usp=sharing)")
     st.write("The training and inference process can be consulted in the\
         notebook [here.](https://github.com/PMMAraujo/HIV-1_recombination_classifier/blob/master/model_creation_and_experimentation.ipynb)")
-    st.write("The imput genomes for this classifier don't need to be previously\
+    st.write("The input genomes for this classifier don't need to be previously\
         aligned.")
     st.write("Since this is a toy example it should not be used for other\
-        purpose than this demonstration.")
+        purposes than this demonstration.")
     st.write("Since this is a toy example information regarding the subtype B\
         was not included in the creation of this classifier.")
